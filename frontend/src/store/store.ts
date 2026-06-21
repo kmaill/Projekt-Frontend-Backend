fbdc1cf930e6ad110c53d849996f1f2797e40b07
bartłomiej drogosz <s096132@student.tu.kielce.pl>
2026-06-21 19:50:00 +0200

    Poprwa błędu z odświerzaniem koszyka

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,11 +2,34 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import cartReducer from './cartSlice';
 
+const loadCartState = () => {
+  try {
+    const serializedState = localStorage.getItem('cartState');
+    if (serializedState === null) {
+      return { items: [], total: 0 };
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return { items: [], total: 0 };
+  }
+};
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
   },
+  preloadedState: {
+    cart: loadCartState()
+  }
+});
+
+store.subscribe(() => {
+  try {
+    const serializedState = JSON.stringify(store.getState().cart);
+    localStorage.setItem('cartState', serializedState);
+  } catch {
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
